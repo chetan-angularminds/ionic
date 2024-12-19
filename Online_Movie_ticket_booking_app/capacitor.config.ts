9aba01b61c364fd1dbcad7eefaa63638ab29eee6
chetan-angularminds <chetan@angularminds.in>
2024-12-19 17:45:16 +0530

    new projects added

---
+++ Online_Movie_ticket_booking_app/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Online_Movie_ticket_booking_app',
+  webDir: 'www'
+};
+
+export default config;
